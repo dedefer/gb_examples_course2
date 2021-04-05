df3db02ca9e3fc822bafeaaa866fddc812c0161f
danila.fomin <danila.fomin@corp.mail.ru>
2021-04-06 02:33:32 +0300

    lesson1 panic_real_example added

--- lesson1/panic_real_example/main.go
+++ lesson1/panic_real_example/main.go
@@ -1,32 +1,129 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"path"
 )
 
-func makePanic() {
-	panic("panic in makePanic")
-}
+var (
+	dataDirPath  = flag.String("data_dir", "lesson1/panic_real_example/data", "data_dir")
+	flockDirPath = flag.String("flock_dir", "lesson1/panic_real_example/flock", "flock_dir")
 
-func callMakePanic() (err error) {
-	defer panicHandlerWithHardLogic(func(e error) { err = e })
+	errFileLocked = errors.New("file locked")
+)
 
-	defer fmt.Println("defer 1 makePanic")
-	defer fmt.Println("defer 2 makePanic")
+func main() {
+	files, err := ioutil.ReadDir(*dataDirPath)
+	if err != nil {
+		fmt.Printf("can't read dir %s: %s\n", *dataDirPath, err)
+		return
+	}
 
-	makePanic()
-	fmt.Println("after makePanic")
-
-	return nil
-}
-
-func panicHandlerWithHardLogic(setter func(error)) {
-	if v := recover(); v != nil {
-		setter(fmt.Errorf("panic occured: %v", v))
+	for _, file := range files {
+		if file.Mode().IsRegular() && path.Ext(file.Name()) == ".db" {
+			fmt.Printf("processing file %s\n", file.Name())
+			if err := process(file); err != nil {
+				if errors.Is(err, errFileLocked) {
+					fmt.Printf("file (%s) locked\n", file.Name())
+				} else {
+					fmt.Printf("error while file (%s) processing: %s\n", file.Name(), err)
+				}
+			}
+		}
 	}
 }
 
-func main() {
-	callMakePanic()
-	fmt.Println("after callMakePanic")
+func process(file os.FileInfo) (err error) {
+	locked, err := lockFile(file.Name())
+	if err != nil { // ошибка при взятии лока
+		return err
+	}
+	if !locked { // кто-то уже обрабатывает файл
+		return errFileLocked
+	}
+
+	// разблокируем файл в конце
+	defer func() {
+		// ловим панику и присваиваем ошибку если была паника
+		if v := recover(); v != nil {
+			err = fmt.Errorf("panic: %w", v)
+		}
+
+		if err != nil { // разблокируем файл при ошибке
+			if unlockErr := unlockFile(file.Name()); unlockErr != nil {
+				fmt.Printf("can't unlock file (%s): %v\n", file.Name(), unlockErr)
+			}
+			return
+		}
+
+		if doneErr := doneFile(file.Name()); doneErr != nil { // помечаем файл обработанным если ошибок не было
+			fmt.Printf("can't mark file (%s) as done: %v\n", file.Name(), doneErr)
+		}
+		if unlockErr := unlockFile(file.Name()); unlockErr != nil {
+			fmt.Printf("can't unlock file (%s): %v\n", file.Name(), unlockErr)
+		}
+	}()
+
+	return internalProcessing(file)
+}
+
+func internalProcessing(file os.FileInfo) error {
+	// Внутреннюю логику пишет большая команда
+	// люди могут и будут ошибаться поэтому
+	// этот код может вызывать панику
+
+	if rand.Intn(100) < 30 {
+		panic(errors.New("don't hug me i'm scared"))
+	}
+	return nil
+}
+
+func lockfileName(name string) string {
+	basename := path.Base(name)
+	return path.Join(*flockDirPath, basename+".lock")
+}
+func donefileName(name string) string {
+	basename := path.Base(name)
+	return path.Join(*flockDirPath, basename+".done")
+}
+
+func lockFile(name string) (ok bool, err error) {
+	_, err = os.Stat(donefileName(name))
+	if err == nil { // файл уже обработан
+		return false, nil
+	}
+
+	if err != nil && !os.IsNotExist(err) { // какая-то ошибка
+		return false, err
+	}
+
+	// проверяем лок файл
+	_, err = os.Stat(lockfileName(name))
+	if err == nil { // лок файл уже существует
+		return false, nil
+	}
+
+	if err != nil && !os.IsNotExist(err) { // какая-то ошибка
+		return false, err
+	}
+
+	if _, err = os.Create(lockfileName(name)); err != nil { // какая-то ошибка при создании лок файла
+		return false, err
+	}
+
+	return true, nil
+}
+
+func unlockFile(name string) error { // Удаляем лок файл
+	return os.Remove(lockfileName(name))
+}
+
+func doneFile(name string) error { // Создаем файл-маркер, что мы закончили
+	_, err := os.Create(donefileName(name))
+	return err
 }
